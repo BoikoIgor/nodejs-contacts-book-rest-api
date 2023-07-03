cb23de32bfe2f7df6735cc8166d767d128f9fd7b
Igor Boiko <igorboiko@gmail.com>
2023-07-03 05:38:18 +0300

    add methods with helpers
    deleteContact NOT working

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 const express = require('express');
-const logger = require('morgan');
+// const logger = require('morgan');
 const cors = require('cors');
 
 const contactsRouter = require('./routes/api/contacts');
 
-const contacts = require('./contacts');
-
 const app = express();
 
-app.listen(3001, console.log('Server listening on port 3001'));
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use((res, req, next) => {
-  console.log('First middleware');
-  next();
-});
-app.get('/contacts', (req, res) => {
-  res.json(contacts);
-  console.log('hi test 1');
-  console.log(req.url);
-  console.log(req.method);
-});
-
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
-
-app.use(logger(formatsLogger));
+// app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
@@ -34,7 +19,8 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,76 @@
 const express = require('express');
-
+const Joi = require('joi');
+const contacts = require('../../models/contacts');
+const { HttpError } = require('../../helpers');
 const router = express.Router();
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'Homework 2 done  message' });
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
-
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      throw HttpError(404, 'Contact not found');
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
-
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' });
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await contacts.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, 'Contact not found');
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.removeContact(id);
+    // console.log(result);
+    if (!result) {
+      throw HttpError(404, 'Contact not found');
+    }
+    res.status(200).json({ message: 'Contact deleted' });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
