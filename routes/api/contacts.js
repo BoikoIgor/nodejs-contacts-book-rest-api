6be2dbae4d69052141e4ae68d3ee7fce415122f3
Igor Boiko <igorboiko@gmail.com>
2023-07-09 01:52:37 +0300

    fix build files of project

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,13 @@
-const express = require('express')
+const express = require('express');
+const ctrl = require('../../controllers/contacts');
+const { validateBody } = require('../../middlewares');
+const schemas = require('../../schemas/contacts.js');
+const router = express.Router();
 
-const router = express.Router()
+router.get('/', ctrl.getAll);
+router.get('/:id', ctrl.getById);
+router.post('/', validateBody(schemas.addSchema), ctrl.add);
+router.put('/:id', validateBody(schemas.updSchema), ctrl.updateById);
+router.delete('/:id', ctrl.deleteById);
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 
-const mongoose = require('mongoose');
-const DB_HOST = 'mongodb+srv://igorboiko:b89d6DntCMeCkH4Q@cluster.ta0xpkl.mongodb.net/db-contacts';
+const DB_HOST = 'mongodb+srv://igorboiko:b89d6DntCMeCkH4Q@cluster.ta0xpkl.mongodb.net/db-contacts?retryWrites=true&w=majority';
+mongoose.set('strictQuery', true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Server running. Use our API on port: 3000');
-    });
+    app.listen(3000);
+    console.log('Server running. Use our API on port: 3000');
   })
   .catch((error) => {
     console.log(error.message);
