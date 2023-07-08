1c98d3125f087f9fd97de2453e0ef28c9a8dcdac
Igor Boiko <igorboiko@gmail.com>
2023-07-09 00:33:52 +0300

    init MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose');
+const DB_HOST = 'mongodb+srv://igorboiko:b89d6DntCMeCkH4Q@cluster.ta0xpkl.mongodb.net/db-contacts';
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log('Server running. Use our API on port: 3000');
+// });
