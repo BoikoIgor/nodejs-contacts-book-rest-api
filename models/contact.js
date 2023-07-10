3d4757a3f8fb28468f1ae8998abef4c23ae9f2a9
Igor Boiko <igorboiko@gmail.com>
2023-07-11 00:22:35 +0300

    move to mongodb

---
+++ models/contact.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const { handleMongooseError } = require('../helpers');
+
+const phoneRegExp = /^\(\d{3}\)\s\d{3}-\d{4}$/;
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      match: phoneRegExp,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+contactSchema.post('save', handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().pattern(phoneRegExp).required(),
+  favorite: Joi.boolean(),
+});
+const updSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = { addSchema, updSchema, updateFavoriteSchema };
+
+const Contact = model('contact', contactSchema);
+
+module.exports = { Contact, schemas };
