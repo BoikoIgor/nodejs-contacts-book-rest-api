77296dde983ee6c7f9e45b02e58f7dc96eda0847
Igor Boiko <igorboiko@gmail.com>
2023-07-15 08:27:01 +0300

    add registration&login routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ctrl = require('../../controllers/contacts');
 const { validateBody, isValidId } = require('../../middlewares');
-const { schemas } = require('../../models/contact');
+const { schemas } = require('../../models/user');
 const router = express.Router();
 
 router.get('/', ctrl.getAll);
